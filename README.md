8449b858da6e79b03d77b5df953bba5550e41a6b
Brayan Gómez <brayan.gomez@sofka.com.co>
2023-10-15 17:34:07 -0500

    finish project

--- README.md
+++ README.md
@@ -39,3 +39,10 @@ curl --location --request GET 'http://localhost:3000/api/v1/seed'
 
 - Nest
 - MongoDB
+
+# Production Build
+
+1. Crear el archivo `.env.prod`
+2. Llenar las variables de entorno de prod
+3. Crear la nueva imagen
+   `docker compose -f docker-compose.prod.yaml --env-file .env.prod up --build`
--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,9 @@ import { JoiValidationSchema } from './common/config/joi.validation';
       validationSchema: JoiValidationSchema,
     }),
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
-    MongooseModule.forRoot(process.env.MONGODB),
+    MongooseModule.forRoot(process.env.MONGODB, {
+      dbName: 'pokemonsdb',
+    }),
     PokemonModule,
     CommonModule,
     SeedModule,
